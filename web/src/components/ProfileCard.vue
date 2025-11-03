164ca21d8f77595b66722abb78d5d1a276d019c5
E99p1ant <i@github.red>
2025-11-04 01:24:36 +0800

    fix avatar image
    Signed-off-by: E99p1ant <i@github.red>

--- web/src/components/ProfileCard.vue
+++ web/src/components/ProfileCard.vue
@@ -3,9 +3,7 @@
     <div class="uk-height-medium uk-flex uk-flex-center uk-flex-middle uk-background-cover uk-light"
          :style="{backgroundImage: `url(${props.background})`}">
       <div class="uk-card-body">
-        <Skeleton circle height="100px" width="100px">
-          <img alt="avatar" class="uk-border-circle uk-box-shadow-large" :src="props.avatar" width="100" height="100"/>
-        </Skeleton>
+        <img alt="avatar" class="uk-border-circle uk-box-shadow-large" :src="props.avatar" width="100" height="100"/>
         <h3>
           <Skeleton>{{ props.name }}</Skeleton>
         </h3>
