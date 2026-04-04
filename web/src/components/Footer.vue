daeafdbc0306815c148b2603d41d1f790303b49c
E99p1ant <i@github.red>
2026-04-04 16:20:23 +0800

    hide pixel
    Signed-off-by: E99p1ant <i@github.red>

--- web/src/components/Footer.vue
+++ web/src/components/Footer.vue
@@ -2,7 +2,7 @@
   <p class="uk-text-right uk-text-muted uk-text-small">
     Made with ❤️ by E99p1ant. {{ icp }}
     <br>
-    <a @click="router.push({name: 'pixel'})">画板</a> |
+<!--    <a @click="router.push({name: 'pixel'})">画板</a> |-->
     <a href="https://github.com/wuhan005/NekoBox" target="_blank">GitHub</a> | <a
       :href="`https://github.com/wuhan005/NekoBox/commit/${commitSHA}`" target="_blank">
     {{ commitSHA.slice(0, 7) }}
